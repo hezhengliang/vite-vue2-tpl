05f0eeb9ddfcc058e126e75a1583dfc3884f0407
M.Winchester <93eryi@gmail.com>
2021-07-21 10:00:59 +0800

    Update env-config.js

--- src/config/env-config.js
+++ src/config/env-config.js
@@ -1,9 +1,9 @@
 const PROTOCOL = window.location.protocol
 export default {
   development: {
-    baseApi: `${PROTOCOL}//test-gateway.hz5800.com`,
+    baseApi: `${PROTOCOL}//xxx.com`,
   },
   production: {
-    baseApi: `${PROTOCOL}//saas.hz5800.com`,
+    baseApi: `${PROTOCOL}//xxx.com`,
   }
 }

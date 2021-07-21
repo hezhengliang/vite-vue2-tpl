3b305f8c00a544646d904e8287e6d4188464af48
M.Winchester <93eryi@gmail.com>
2021-07-21 13:25:37 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,6 @@
 </template>
 <script>
 import { onMounted, getCurrentInstance } from 'vue'
-import { saBuryPageView } from '@/utils/sa-bury.js'
-// const comFunc = require('@/utils/comonjs-test.js')
-// console.log(comFunc)
 export default {
   setup(){
     const {proxy} = getCurrentInstance()
